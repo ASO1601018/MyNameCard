389bed0e666ae7e90008971c0a8fded172aa24a9
ASO1601018 <32320589+ASO1601018@users.noreply.github.com>
2017-09-27 15:06:00 +0900

    Initial commit

---
+++ app/src/main/java/com/example/masahiro/mynamecard/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.masahiro.mynamecard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button tapHere = (Button)findViewById(R.id.name);
+        tapHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                TextView textView = (TextView)findViewById(R.id.textView2);
+                textView.setText("なかにし");
+            }
+        });
+    }
+}
